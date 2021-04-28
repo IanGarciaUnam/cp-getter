d9f4db618039d51d463ec4f7718f437d11c4274d
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2021-04-28 13:58:18 -0500

    Last commit

---
+++ cp-getter.py
@@ -0,0 +1,28 @@
+import xlrd,sys
+
+def get_data_from_xlsx(file_path:str,column:int):
+	cp_list:list=[]
+	loc= file_path
+	wb=xlrd.open_workbook(loc)
+	sheet=wb.sheet_by_index(0)
+	x=5
+	for x in range(sheet.nrows):
+		try:
+			cp_list.append(int(sheet.cell_value(x,column)))
+		except ValueError:
+			continue
+
+	return cp_list
+
+def write_txt(cp:list):
+	content=""
+	for c in cp:
+		content+=str(c)+"\n"
+	textfile=open("file.txt","w")
+	textfile.write(content)
+	textfile.close()
+
+
+if __name__=="__main__":
+	file_path=sys.argv[1]
+	write_txt(get_data_from_xlsx(file_path, 0))
